eddd05d4815be68d6da9b042b53270069bf08c25
MatiasCordich <m.scordich@gmail.com>
2022-02-01 22:12:23 -0300

    portfolio v2 proyects

--- src/pages/Proyects.jsx
+++ src/pages/Proyects.jsx
@@ -1,8 +1,107 @@
-import React from 'react';
+import './proyects.css'
 
 const Proyects = () => {
   return <div>
       <h1>Proyects</h1>
+      <div className='cards'>
+        <div className='card'>
+          <div className='face face1'>
+            <div className='content'>
+              <div className='content__image'></div>
+              <h3>Proyect 1</h3>
+            </div>
+          </div>
+          <div className='face face2'>
+            <div className='content'>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius molestiae maiores labore numquam autem repellat ipsum distinctio alias   recusandae exercitationem reiciendis, a error at beatae soluta quos aut ratione quis.</p>
+              <a href="#">A</a>
+              <a href="#">A</a>
+            </div>
+          </div>
+        </div>
+  
+        <div className='card'>
+          <div className='face face1'>
+            <div className='content'>
+              <div className='content__image'></div>
+              <h3>Proyect 1</h3>
+            </div>
+          </div>
+          <div className='face face2'>
+            <div className='content'>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius molestiae maiores labore numquam autem repellat ipsum distinctio alias   recusandae exercitationem reiciendis, a error at beatae soluta quos aut ratione quis.</p>
+              <a href="#">A</a>
+              <a href="#">A</a>
+            </div>
+          </div>
+        </div>
+  
+        <div className='card'>
+          <div className='face face1'>
+            <div className='content'>
+              <div className='content__image'></div>
+              <h3>Proyect 1</h3>
+            </div>
+          </div>
+          <div className='face face2'>
+            <div className='content'>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius molestiae maiores labore numquam autem repellat ipsum distinctio alias   recusandae exercitationem reiciendis, a error at beatae soluta quos aut ratione quis.</p>
+              <a href="#">A</a>
+              <a href="#">A</a>
+            </div>
+          </div>
+        </div>
+  
+        <div className='card'>
+          <div className='face face1'>
+            <div className='content'>
+              <div className='content__image'></div>
+              <h3>Proyect 1</h3>
+            </div>
+          </div>
+          <div className='face face2'>
+            <div className='content'>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius molestiae maiores labore numquam autem repellat ipsum distinctio alias   recusandae exercitationem reiciendis, a error at beatae soluta quos aut ratione quis.</p>
+              <a href="#">A</a>
+              <a href="#">A</a>
+            </div>
+          </div>
+        </div>
+  
+        <div className='card'>
+          <div className='face face1'>
+            <div className='content'>
+              <div className='content__image'></div>
+              <h3>Proyect 1</h3>
+            </div>
+          </div>
+          <div className='face face2'>
+            <div className='content'>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius molestiae maiores labore numquam autem repellat ipsum distinctio alias   recusandae exercitationem reiciendis, a error at beatae soluta quos aut ratione quis.</p>
+              <a href="#">A</a>
+              <a href="#">A</a>
+            </div>
+          </div>
+        </div>
+  
+        <div className='card'>
+          <div className='face face1'>
+            <div className='content'>
+              <div className='content__image'></div>
+              <h3>Proyect 1</h3>
+            </div>
+          </div>
+          <div className='face face2'>
+            <div className='content'>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius molestiae maiores labore numquam autem repellat ipsum distinctio alias   recusandae exercitationem reiciendis, a error at beatae soluta quos aut ratione quis.</p>
+              <a href="#">A</a>
+              <a href="#">A</a>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
   </div>;
 };
 
