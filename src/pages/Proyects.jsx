0dd4bd7fe4186b1399381f85af88ca2a63f6a0d6
MatiasCordich <m.scordich@gmail.com>
2022-02-16 22:41:55 -0300

    proyects section changes

--- src/pages/Proyects.jsx
+++ src/pages/Proyects.jsx
@@ -1,5 +1,4 @@
 import './proyects.css'
-import calculadora from '../images/calculadora.png'
 
 const Proyects = () => {
   return <section className='proyects' data-aos="fade-down" data-aos-duration="1500" data-aos-delay="500">
@@ -18,10 +17,10 @@ const Proyects = () => {
               <p className='description-text'>Calculadora funcional hecha con Javascript vanilla</p>
               <p className='description-tech'>HTML | CSS | JS</p>
               <div className='btn__proyects'>
-                <a href="#">
+                <a href="https://calculadora-lemon-rho.vercel.app/" target='__blank'>
                   <i class="fas fa-link"></i>
                 </a>
-                <a href="#">
+                <a href="https://github.com/MatiasCordich/Calculadora" target='__blank'>
                   <i class="fab fa-github"></i>
                 </a>
               </div>
@@ -40,10 +39,10 @@ const Proyects = () => {
               <p className='description-text'>Landing Page sobre una ecommerce de quesos</p>
               <p className='description-tech'>HTML | CSS </p>
               <div className='btn__proyects'>
-                <a href="#">
+                <a href="https://pagina-quesos.vercel.app/" target='__blank'>
                   <i class="fas fa-link"></i>
                 </a>
-                <a href="#">
+                <a href="https://github.com/MatiasCordich/Pagina-Quesos" target='__blank'>
                   <i class="fab fa-github"></i>
                 </a>
               </div>
@@ -53,19 +52,19 @@ const Proyects = () => {
 
         <div className='card'>
           <div className='face face1'>
-            <div className='content'>
+            <div className='content-2'>
             </div>
           </div>
           <div className='face face2'>
             <div className='content'>
               <h2 className='description-title'>Pokemon Random</h2>
               <p className='description-text'>App que arroja un pokemon random con sus stats.</p>
-              <p className='description-tech'>HTML | CSS | JS | PokeApi | React</p>
+              <p className='description-tech'>React | PokeApi</p>
               <div className='btn__proyects'>
-                <a href="#">
+                <a href="https://poke-api-xi.vercel.app/" target='__blank'>
                   <i class="fas fa-link"></i>
                 </a>
-                <a href="#">
+                <a href="https://github.com/MatiasCordich/PokeApi" target='__blank'>
                   <i class="fab fa-github"></i>
                 </a>
               </div>
@@ -75,7 +74,7 @@ const Proyects = () => {
 
         <div className='card'>
           <div className='face face1'>
-            <div className='content'>
+            <div className='content-3'>
             </div>
           </div>
           <div className='face face2'>
@@ -84,10 +83,10 @@ const Proyects = () => {
               <p className='description-text'>Información meteorológica de tu ubicación actual</p>
               <p className='description-tech'>HTML | CSS | JS | WeatherAPI</p>
               <div className='btn__proyects'>
-                <a href="#">
+                <a href="https://weather-app-alpha-seven.vercel.app/" target='__blank'>
                   <i class="fas fa-link"></i>
                 </a>
-                <a href="#">
+                <a href="https://github.com/MatiasCordich/Weather-App" target='__blank'>
                   <i class="fab fa-github"></i>
                 </a>
               </div>
