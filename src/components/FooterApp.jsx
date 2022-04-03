a7468876022dd35e787c9895b60432be8c3f9418
MatiasCordich <m.scordich@gmail.com>
2022-04-03 13:56:37 -0300

    year and title modified

--- src/components/FooterApp.jsx
+++ src/components/FooterApp.jsx
@@ -1,9 +1,12 @@
 import './footer.css'
 
 const FooterApp = () => {
+
+  let year = new Date().getFullYear()
+
   return <footer className='footer'>
 
-    <h4>Matias Sio Cordich <span>&#169;</span> 2022</h4>
+    <h4>Matias Sio Cordich <span>&#169;</span> {year}</h4>
     <p>
       Made with <i class="fas fa-heart"></i>
     </p>
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,49 +1,61 @@
-import './homepage.css'
-import image from '../images/matias.png'
-import pdf from '../files/CV.pdf'
+import "./homepage.css";
+import image from "../images/matias.png";
+import pdf from "../files/CV.pdf";
 
 const HomePage = () => {
   return (
-    <section className='home' data-aos="fade-up"
-    data-aos-duration="1500" data-aos-delay="500">
-        <div className='home__image'>
-          <div className='circle'>
-            <img className='circle__image' src={image} alt="matias" />
-          </div>
+    <section
+      className="home"
+      data-aos="fade-up"
+      data-aos-duration="1500"
+      data-aos-delay="500"
+    >
+      <div className="home__image">
+        <div className="circle">
+          <img className="circle__image" src={image} alt="matias" />
         </div>
-        <div className='home__content'>
-            <div className='home__text'>
-              <h1>Hola</h1>
-              <h2>Me llamo Matias</h2>
-              <p>
-                Soy <span>Full Stack Developer</span> orientado al <span>Front End</span> y trabajando con el stack <span>MERN</span>. Actualmente me encuentro realizando mis propios proyectos.
-              </p>
-            </div>
-            <div className='home__btn-social'>
-
-              <a href="https://github.com/MatiasCordich" target="_blank" rel='noreferrer'>
-                <i className="fab fa-github"></i>
-              </a>
-
-              <a href="https://www.linkedin.com/in/matiassiocordich/" target="_blank" rel='noreferrer'>
-                <i className="fab fa-linkedin-in"></i>
-              </a>
-
-            </div>
-
-            <div className='home__btn-contact'>
-              <a href="mailto:m.cordich20@gmail.com">
-                 <i className="far fa-envelope"></i> Contacto
-              </a>
-              <a href={pdf} target="_blank" rel='noreferrer'>
-                <i className="far fa-file"></i> Mi CV
-              </a>
-            </div>
+      </div>
+      <div className="home__content">
+        <div className="home__text">
+          <h3>Hola</h3>
+          <h2>
+            Me llamo <span>Matias</span>{" "}
+          </h2>
+          <p>
+            Soy <span>Full Stack Developer</span> orientado al{" "}
+            <span>Front End</span> y trabajando con el stack <span>MERN</span>.
+            Actualmente me encuentro realizando mis propios proyectos.
+          </p>
         </div>
-        
+        <div className="home__btn-social">
+          <a
+            href="https://github.com/MatiasCordich"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-github"></i>
+          </a>
 
+          <a
+            href="https://www.linkedin.com/in/matiassiocordich/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <i className="fab fa-linkedin-in"></i>
+          </a>
+        </div>
+
+        <div className="home__btn-contact">
+          <a href="mailto:m.cordich20@gmail.com">
+            <i className="far fa-envelope"></i> Contacto
+          </a>
+          <a href={pdf} target="_blank" rel="noreferrer">
+            <i className="far fa-file"></i> Mi CV
+          </a>
+        </div>
+      </div>
     </section>
-  ) 
+  );
 };
 
 export default HomePage;
